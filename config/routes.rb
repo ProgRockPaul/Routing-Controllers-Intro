776e86269ada0f29b9a8ed1f165ae1f0c2a3d61f
Paul Walsh <twentytwentyk@live.com>
2017-01-30 14:27:09 -0500

    got secret route working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get '/welcome' => 'pages#welcome'
+  get '/' => 'pages#welcome'
+  get '/about' => 'pages#about'
+  get '/contest' => 'pages#contest'
+  get '/kitten/:size/:size' => 'pages#kitten'
+  get '/kittens/:size/:size' => 'pages#kittens'
+  get '/secrets/:magic_word' => 'pages#secrets'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
